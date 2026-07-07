6efd3eda7dcc455dc2f1560ad94a10d0ba8c5d03
Magnoliar <18226972+Magnoliar@users.noreply.github.com>
2026-07-07 18:07:40 +0800

    Use Nitro plugin for line art cleanup

--- server/plugins/line-art-cleanup.ts
+++ server/plugins/line-art-cleanup.ts
@@ -1,4 +1,6 @@
-export default defineNuxtPlugin(() => {
+import { markStaleRunningJobs } from '../utils/line-art-jobs'
+
+export default defineNitroPlugin(() => {
   // 服务启动时清理上次运行遗留的过期任务
   markStaleRunningJobs().catch((err: any) => {
     console.warn('Failed to mark stale line-art jobs:', err?.message)
